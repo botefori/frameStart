23e171a7a62a572005845dc9c34754cf8648c2ae
Ibrahima CAMARA <icamara@wynd.eu>
2018-01-30 10:55:56 +0100

    update the defauld app file to merge code from testing the framework first use

--- framework/src/app.php
+++ framework/src/app.php
@@ -3,7 +3,29 @@
 use Symfony\Component\Routing;
 use Symfony\Component\Routing\Route;
 
+function is_leap_year($year = null)
+{
+    if(null === $year)
+    {
+        $year = date('Y');
+    }
+    return 0 === $year % 400 || (0 === $year % 4 && 0 !== $year % 100);
+}
+
 $routes = new Routing\RouteCollection();
+
+$routes->add('leap_year', new Route('/is_leap_year/{year}', [
+        'year' => null,
+        '_controller' => function($request)
+        {
+            if(is_leap_year($request->attributes->get('year'))){
+                return new Response('Yep, this year is a leap!');
+            }
+
+            return new Response('Nope, this is not a leap year.');
+        }
+    ]
+));
 
 $routes->add('hello', new Route('/hello/{name}', array('name' => 'world',
                                                                     '_controller' => function($request)
