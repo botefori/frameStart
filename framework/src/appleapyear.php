e5af3927e2d457c025a354fe3746642349ccb6b5
Ibrahima CAMARA <icamara@wynd.eu>
2018-02-02 10:50:41 +0100

    install xdebug php-extension for phpunit codecoverage purpose and clean the code"

--- framework/src/appleapyear.php
+++ framework/src/appleapyear.php
@@ -4,15 +4,6 @@ use Symfony\Component\Routing;
 use Symfony\Component\Routing\Route;
 use Symfony\Component\HttpFoundation\Response;
 
-function is_leap_year($year = null)
-{
-    if(null === $year)
-    {
-        $year = date('Y');
-    }
-    return 0 === $year % 400 || (0 === $year % 4 && 0 !== $year % 100);
-}
-
 $routes =  new Routing\RouteCollection();
 
 $routes->add('leap_year', new Route('/is_leap_year/{year}', [
